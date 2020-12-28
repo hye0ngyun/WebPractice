cd18031d6406b3c7e1589dc30e5937c6496785c3
Hyeongyun <shg7271@gmail.com>
2020-12-28 23:04:46 +0900

    learn js: object

--- JavaScript/dream_coding/javascript/object.js
+++ JavaScript/dream_coding/javascript/object.js
@@ -35,5 +35,74 @@ ellie['hasJob'] = true;
 console.log(ellie.hasJob);
 
 function printValue(obj, key){
-    console.log(obj, );
+    console.log(obj[key]);
 }
+printValue(ellie, 'name');
+printValue(ellie, 'age');
+
+// 3. Property value shorthand
+const person1 = { name: 'bob', age: 2};
+const person2 = { name: 'park', age: 3};
+const person3 = { name: 'davee', age: 4};
+const person4 = new Person('ellie', 30);
+console.log(person4);
+
+// 4. Constuctor function
+function Person(name, age){
+    // this = {};
+    this.name = name;
+    this.age = age;
+    // return this
+}
+
+// 5. in Operator: property existence check (key in obj)
+console.log('name' in ellie);
+console.log('age' in ellie);
+console.log('random' in ellie);
+console.log(ellie.random);
+
+// 6. for .. in vs for .. of
+// for (key in obj)
+console.clear();
+for(key in ellie){
+    console.log(key);
+}
+
+// for (value of iterable)
+const array = [1,2,4,5];
+// for(let i = 0; i < array.length; i++){
+//     console.log(array[i]);
+// }
+for(value of array){
+    console.log(value);
+}
+
+// 7. Fun cloning
+// Object.assign(dest, [obj1, obj2, obj3...])
+const user = { name: 'ellie', age: '20' };
+const user2 = user;
+console.log(user);
+user2.name = 'coder';
+console.log(user);
+
+// old way
+const user3 = {};
+for(key in user){
+    user3[key] = user[key];
+}
+console.clear();
+console.log(user3);
+user3.name = 'shin';
+console.log(user);
+console.log(user3);
+
+// const user4 = {}
+// Object.assign(user4, user);
+const user4 = Object.assign({}, user);
+console.log(user4);
+
+// another example
+const fruit1 = {color: 'red'};
+const fruit2 = {color: 'blue', size: 'big'};
+const mixed = Object.assign({}, fruit1, fruit2);
+console.log(mixed);
