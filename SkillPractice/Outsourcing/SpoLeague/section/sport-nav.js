1198344c98a28e21bbca0e820409c47b795b33a1
Hyeongyun <shg7271@gmail.com>
2021-02-07 20:29:51 +0900

    Create sport-sportgame.html

---
+++ SkillPractice/Outsourcing/SpoLeague/section/sport-nav.js
@@ -0,0 +1,9 @@
+let sport = `
+<div class="main-header">
+                <a href="" class="orange-border">스포츠게임</a><div class="border"></div>
+                <a href="">라이브게임</a><div class="border"></div>
+                <a href="">게임내역</a><div class="border"></div>
+                <a href="">게임결과</a>
+            </div>
+`
+document.write(sport);
