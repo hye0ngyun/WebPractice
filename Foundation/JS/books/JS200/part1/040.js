db3fafd70250d5b01257ddcaa69675590cfa3e86
Hyeongyun <shg7271@gmail.com>
2022-01-17 23:12:50 +0900

    Learn js: JS200 - 040

---
+++ Foundation/JS/books/JS200/part1/040.js
@@ -0,0 +1,12 @@
+// 함수 호이스팅 이해하기
+console.log(hello); // [Function: hello]
+hello(); // 안녕하세요
+function hello() {
+  console.log('안녕하세요');
+}
+
+console.log(hello2); // undefined
+hello2(); // hello2 is not function
+var hello2 = function () {
+  console.log('안녕하세요2');
+}
