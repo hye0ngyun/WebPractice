113074aedf2e588ed8503282004e848490c15376
Shin <shg7271@gmail.com>
2022-06-09 20:28:17 +0900

    Learn js: Momentum chap4 Login

--- Foundation/JS/lectures/youtube/nomad_coder/vanillajs/2.0/app.js
+++ Foundation/JS/lectures/youtube/nomad_coder/vanillajs/2.0/app.js
@@ -1,1 +1,29 @@
-alert('hi')
+const loginForm = document.querySelector("#login-form");
+// 선택된 loginForm 또한 document의 하위 객체이므로 querySelector함수를 사용할 수 있다.
+const loginInput = loginForm.querySelector("input"); // #login-form input
+const loginButton = document.querySelector("#login-form button");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "userName";
+const greeting = document.querySelector("#greeting");
+
+const svaedUserName = localStorage.getItem(USERNAME_KEY);
+
+function onLoginSubmit(e) {
+  e.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  localStorage.setItem(USERNAME_KEY, loginInput.value);
+  paintGreetings();
+}
+
+function paintGreetings() {
+  const userName = localStorage.getItem(USERNAME_KEY);
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+  greeting.innerText = `Hello ${userName}`;
+}
+if (svaedUserName === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  paintGreetings();
+}
