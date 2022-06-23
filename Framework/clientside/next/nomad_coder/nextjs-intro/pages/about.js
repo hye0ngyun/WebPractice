3b2c3bc7091544ec3003399f6b062aef868c782c
Shin <shg7271@gmail.com>
2022-06-23 11:31:59 +0900

    Learn next: react framework basic, SSR, Router

---
+++ Framework/clientside/next/nomad_coder/nextjs-intro/pages/about.js
@@ -0,0 +1,10 @@
+import NavBar from '../components/NavBar';
+
+export default function About() {
+  return (
+    <div>
+      <NavBar />
+      <h2>About</h2>
+    </div>
+  );
+}
---
+++ Framework/clientside/next/nomad_coder/nextjs-intro/pages/index.js
@@ -0,0 +1,10 @@
+import NavBar from "../components/NavBar";
+
+export default function Home() {
+  return (
+    <div>
+      <NavBar />
+      <h2>Hello</h2>
+    </div>
+  );
+}
