16244fc27ee714c2e904490ff40bb199bbfe8a71
Shin <shg7271@gmail.com>
2022-06-23 12:04:07 +0900

    Learn next: styles JSX

--- Framework/clientside/next/nomad_coder/nextjs-intro/components/NavBar.js
+++ Framework/clientside/next/nomad_coder/nextjs-intro/components/NavBar.js
@@ -1,30 +1,28 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import styles from "./NavBar.module.css";
 export default function NavBar() {
   const router = useRouter();
-  console.log(router);
   return (
     <nav>
       <Link href="/">
-        <a
-          className={`${router.pathname === "/" ? styles.active : ""} ${
-            styles.link
-          }`}
-        >
-          Home
-        </a>
+        <a className={router.pathname === "/" ? "active" : ""}>Home</a>
       </Link>
       <Link href="/about">
-        <a
-          className={[
-            router.pathname === "/about" ? styles.active : "",
-            styles.link,
-          ].join(" ")}
-        >
-          About
-        </a>
+        <a className={router.pathname === "/about" ? "active" : ""}>About</a>
       </Link>
+      <style jsx>
+        {`
+          nav {
+            background-color: tomato;
+          }
+          a {
+            text-decoration: none;
+          }
+          .active {
+            color: blue;
+          }
+        `}
+      </style>
     </nav>
   );
 }
--- Framework/clientside/next/nomad_coder/nextjs-intro/pages/about.js
+++ Framework/clientside/next/nomad_coder/nextjs-intro/pages/about.js
@@ -1,10 +1,15 @@
-import NavBar from '../components/NavBar';
+import NavBar from "../components/NavBar";
 
 export default function About() {
   return (
     <div>
       <NavBar />
-      <h2>About</h2>
+      <h2 className="active">About</h2>
+      <style jsx>{`
+        .active {
+          color: red;
+        }
+      `}</style>
     </div>
   );
 }
